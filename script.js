96465c272991538fdbb8791246e5f41f2c4b32b9
CR4CKSTEIN <rico47198@live.de>
2024-01-28 07:41:08 +0100

    dev 0.0.4

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+const wait_for = async (conditional, interval = 20) => {
+  return new Promise((resolve) => {
+    const _wait_for_interval = setInterval(() => {
+      if (conditional() === true) {
+        clearInterval(_wait_for_interval);
+        resolve();
+      }
+    }, interval);
+  });
+};
+
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 
 const autoScroll = () => {
   window.scrollBy(0, 1000);
@@ -13,10 +25,23 @@ const autoScroll = () => {
 autoScroll();
 
 const function2 = async () => {
+  await wait_for(
+    () =>
+      document.querySelector(
+        'ytd-menu-service-item-renderer[class="style-scope ytd-menu-popup-renderer"][role="menuitem"]'
+      ) !== null
+  );
+
   const buttons = document.querySelectorAll(
     'ytd-menu-service-item-renderer[class="style-scope ytd-menu-popup-renderer"][role="menuitem"]'
   );
   for (const button of buttons) {
+    await wait_for(
+      () =>
+        document.querySelector(
+          'yt-formatted-string[class="style-scope ytd-menu-service-item-renderer"]'
+        ) !== null
+    );
     const perfectButton = button.querySelector(
       'yt-formatted-string[class="style-scope ytd-menu-service-item-renderer"]'
     );
@@ -32,11 +57,23 @@ const function2 = async () => {
 };
 
 const function1 = async () => {
+  await wait_for(
+    () =>
+      document.querySelector(
+        "yt-icon-button[id='button'][class='dropdown-trigger style-scope ytd-menu-renderer']"
+      ) !== null
+  );
   const videos = document.querySelectorAll(
     "yt-icon-button[id='button'][class='dropdown-trigger style-scope ytd-menu-renderer']"
   );
 
   for (const video of videos) {
+    await wait_for(
+      () =>
+        document.querySelector(
+          "button[id='button'][class='style-scope yt-icon-button'][aria-label]"
+        ) !== null
+    );
     const test = video.querySelector(
       "button[id='button'][class='style-scope yt-icon-button'][aria-label]"
     );
