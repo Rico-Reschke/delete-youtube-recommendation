cb58a03fde5e0d25b06ba990d8fd19abff630da0
CR4CKSTEIN <rico47198@live.de>
2024-01-28 20:34:17 +0100

    dev 0.0.7

--- script.js
+++ script.js
@@ -9,25 +9,33 @@ const zoomEffect = async () => {
   await sleep(1000); // Kurze Pause bei 25% Zoom
 
   document.body.style.zoom = "100%"; // Zurücksetzen auf normalen Zoom
+  await sleep(1000);
 };
 
 const autoScroll = async () => {
   let lastHeight = document.documentElement.scrollHeight;
   let attempt = 0;
+  let scrollAttempt = 0;
 
   while (true) {
-    window.scrollBy(0, 1000); // Scroll um einen festgelegten Wert nach unten
+    window.scrollBy(0, 1000);
     console.log("Gescrollt");
+    scrollAttempt++;
 
-    // Warten Sie eine Weile, um das Nachladen von Inhalten zu ermöglichen
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise(resolve => setTimeout(resolve, 2000)); 
 
     const currentHeight = document.documentElement.scrollHeight;
     if (lastHeight === currentHeight) {
-      // Wenn die Höhe gleich bleibt, versuchen wir es noch ein paar Mal, bevor wir aufhören
       if (attempt++ > 3) {
-        // Versuchen Sie es z.B. 3 Mal
-        break;
+        break; // Beenden, wenn die Höhe sich nach mehreren Versuchen nicht ändert
+      }
+      if (scrollAttempt === 2) {
+        // Klicken Sie auf die Navbar, wenn Probleme beim zweiten Scroll-Versuch auftreten
+        const navbar = document.querySelector('div[id="container"][class="style-scope ytd-masthead"]');
+        if (navbar) {
+          console.log("Klicke auf die Navbar");
+          navbar.click();
+        }
       }
     } else {
       lastHeight = currentHeight;
@@ -35,27 +43,29 @@ const autoScroll = async () => {
     }
   }
 
-  console.log(
-    "Das Ende der Seite wurde erreicht oder keine neuen Inhalte mehr geladen"
-  );
+  console.log("Das Ende der Seite wurde erreicht oder keine neuen Inhalte mehr geladen");
 };
+
 
 const deleteSubmitClick = async () => {
   const buttons = document.querySelectorAll(
     'ytd-menu-service-item-renderer[class="style-scope ytd-menu-popup-renderer"][role="menuitem"]'
   );
 
-  // Prüfen, ob mindestens fünf Elemente vorhanden sind
   if (buttons.length >= 5) {
-    // Zugriff auf das fünfte Element (Index 4)
     const fifthButton = buttons[4].querySelector(
       'yt-formatted-string[class="style-scope ytd-menu-service-item-renderer"]'
     );
     console.log("Fünftes Element:", fifthButton);
     fifthButton.click();
-    sleep(1000);
+    await sleep(1000);
   } else {
     console.log("Weniger als fünf Elemente gefunden.");
+    const button = document.querySelector(
+      'tp-yt-paper-item[class="style-scope ytd-menu-service-item-renderer"][style-target="host"]'
+    );
+    button.click();
+    await sleep(1000);
   }
 };
 
@@ -72,6 +82,7 @@ const dropdownSubmitClick = async () => {
       console.log("Klicke auf das Video-Element", dropdown);
       dropdown.click();
       await deleteSubmitClick();
+      await sleep(1000);
     }
   }
 };
@@ -80,6 +91,8 @@ const bypass = async () => {
   await autoScroll();
   await dropdownSubmitClick();
   await zoomEffect();
+  await sleep(3000);
+  await bypass();
 };
 
 bypass();
